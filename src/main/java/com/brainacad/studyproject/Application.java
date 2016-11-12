f0b3898a1ba9a95def8f9c2f6cc7a988045b69f5
UArabei <karpynnv@gmail.com>
2016-11-12 12:27:57 +0200

    new changes

---
+++ src/main/java/com/brainacad/studyproject/Application.java
@@ -0,0 +1,28 @@
+package com.brainacad.studyproject;
+
+import com.brainacad.studyproject.data.core.StubDataHolder;
+import com.brainacad.studyproject.data.dao.impl.JdbcUserDao;
+import com.brainacad.studyproject.data.domain.User;
+import com.brainacad.studyproject.gui.view.ViewRouter;
+import com.brainacad.studyproject.service.impl.LoginServiceImpl;
+
+import java.awt.*;
+import java.sql.*;
+import java.util.Date;
+
+public class Application {
+
+    public static void main(String[] args) {
+        StubDataHolder.createData();
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                ViewRouter viewRouter = ViewRouter.getInstance();
+                viewRouter.start();
+                //adasdad
+            }
+        });
+    }
+
+}
+
+
